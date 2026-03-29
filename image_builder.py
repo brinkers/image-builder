f4997b9cf0f9024a289f04693b51af7a24b65d59
stephen brinkworth <brinkers@outlook.com>
2026-03-29 15:03:56 +1100

    Initial commit

---
+++ image_builder.py
@@ -0,0 +1,69 @@
+from typing import Any
+from wsgiref import headers
+
+import requests
+
+
+class ImageBuilderClient:
+    BASE = "https://console.redhat.com/api/image-builder/v1"
+
+    def __init__(self, token_key=None):
+        self.token_key = token_key
+
+    def _call_api(self, method: str, endpoint: str, **params) -> Any:
+        headers = {"Content-Type": "application/json"}
+        if self.token_key:
+            headers.update({"Authorization": f"Bearer {self.token_key}"})
+        response  = requests.request(method, f"{self.BASE}/{endpoint}", params=params, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    def _get(self, endpoint, **params):
+        return self._call_api("GET", endpoint, **params)
+
+    def _post(self, endpoint, **params):
+        return self._call_api("POST", endpoint, **params)
+
+
+    def get_something(self, query):
+        data = self._get("search", q=query)
+        return [{"title": r["title"], "url": r["url"]} for r in data["results"]]
+
+    def get_version(self):
+        return self._get("version")
+
+    def get_readiness(self):
+        return self._get("ready")
+
+    def get_distributions(self):
+        return self._get("distributions")
+    
+    def get_blueprints(self):
+        return self._get("blueprints")
+    
+    def create_compose_from_blueprint(self, blueprint_id):
+        return self._post(f"blueprints/{blueprint_id}/compose")
+
+    def create_compose(self, name, distribution, data):
+        payload = {
+            "image_name": name,
+            "distribution": distribution,
+            "image_requests": [
+                {
+                    "architecture": "x86_64",
+                    "image_type":  "vsphere",
+                    "upload_request": {"type": "aws.s3", "options": {}},
+                }
+            ],
+        }
+        headers = {"Content-Type": "application/json"}
+        if self.token_key:
+            headers.update({"Authorization": f"Bearer {self.token_key}"})   
+        resp = requests.post(f"{self.BASE}/compose", json=payload, headers=headers)
+        resp.raise_for_status()
+        return resp.json()
+
+
+    def get_compose_status(self, compose_id):
+        return self._get(f"composes/{compose_id}")
+        
---
+++ main.py
@@ -0,0 +1,80 @@
+
+import time
+
+import requests
+import dotenv
+import os
+import json
+import image_builder
+
+dotenv.load_dotenv()
+TOKEN = os.environ["TOKEN"]
+
+url = "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token"
+
+payload = {
+    "grant_type": "refresh_token",
+    "client_id": "rhsm-api",
+    "refresh_token": TOKEN,
+}
+
+response = requests.post(url, data=payload)
+response.raise_for_status()  
+
+access_token = response.json().get("access_token")
+
+api_client = image_builder.ImageBuilderClient(token_key=access_token)
+
+blueprints = api_client.get_blueprints()
+
+print(json.dumps(blueprints, indent=4))
+
+bp  = [bp for bp in blueprints["data"] if bp["name"] == "StandardImage"][0]
+
+print(json.dumps(bp, indent=4))
+
+compose = api_client.create_compose_from_blueprint(bp["id"])
+
+print(json.dumps(compose, indent=4))
+
+id = compose[0]["id"]
+print(f"Compose ID: {id}")
+
+status = api_client.get_compose_status(id)
+print(json.dumps(status, indent=4))
+
+sleep_time = 30
+while status['image_status']['status'] not in ["success", "failure"]:
+    print(f"Compose status: {status['image_status']['status']}. Checking again in {sleep_time} seconds...")
+    time.sleep(sleep_time)
+    status = api_client.get_compose_status(id)  
+    
+print(f"Final compose status: {status['image_status']['status']}")
+
+print(json.dumps(status, indent=4))
+# r = api_client.create_compose(
+#     name="My_image", 
+#     distribution="rhel-90",
+#     data={}
+# )
+
+
+
+# id = r["id"]
+# print(f"Compose ID: {id}")
+
+
+# status = api_client.get_compose_status(id)
+# print(json.dumps(status, indent=4))
+
+# sleep_time = 30
+# while status['image_status']['status'] not in ["success", "failure"]:
+#     print(f"Compose status: {status['image_status']['status']}. Checking again in {sleep_time} seconds...")
+#     time.sleep(sleep_time)
+#     status = api_client.get_compose_status(id)  
+    
+
+      
+
+
+
